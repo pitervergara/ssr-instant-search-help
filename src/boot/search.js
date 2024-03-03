36de7a882e96808b24284aa8110a8556254228ec
Piter Vergara <pitervergara@users.noreply.github.com>
2024-03-03 19:31:50 +0000

    Clean

--- src/boot/search.js
+++ src/boot/search.js
@@ -3,36 +3,21 @@ import VueInstantSearch, {
   createServerRootMixin,
 } from "vue-instantsearch/vue3/es";
 import algoliasearch from "algoliasearch/lite";
-import { ref } from "vue";
-import { history as historyRouter } from "instantsearch.js/es/lib/routers";
-import { createWidgetMixin } from "vue-instantsearch/vue3/es";
 
 export default boot(async ({ app, ssrContext }) => {
   app.use(VueInstantSearch);
-
-  const url = ssrContext ? ssrContext.url : window.location.href;
-  const routing = {
-    router: historyRouter({
-      getLocation() {
-        return url;
-      },
-      cleanUrlOnDispose: false,
-    }),
-  };
 
   const searchClient = algoliasearch(
     "latency",
     "6be0576ff61c053d5f9a3225e2a90f76"
   );
 
-  const serverRootMixin = ref(
-    createServerRootMixin({
-      searchClient,
-      indexName: "instant_search",
-    })
-  );
+  const serverRootMixin = createServerRootMixin({
+    searchClient,
+    indexName: "instant_search",
+  });
 
-  const instantsearch = serverRootMixin.value.data()["instantsearch"];
+  const instantsearch = serverRootMixin.data()["instantsearch"];
   app.provide("serverRootMixin", serverRootMixin);
   app.provide("$_ais_ssrInstantSearchInstance", instantsearch);
 
---
+++ src/composables/useRouting.js
@@ -0,0 +1,115 @@
+import { history as historyRouter } from "instantsearch.js/es/lib/routers";
+import { simple as simpleStateMapping } from "instantsearch.js/es/lib/stateMappings";
+
+// Returns a slug from the category name.
+// Spaces are replaced by "+" to make
+// the URL easier to read and other
+// characters are encoded.
+function getCategorySlug(name) {
+  return name.split(" ").map(encodeURIComponent).join("+");
+}
+
+// Returns a name from the category slug.
+// The "+" are replaced by spaces and other
+// characters are decoded.
+function getCategoryName(slug) {
+  return slug.split("+").map(decodeURIComponent).join(" ");
+}
+
+const routing = {
+  router: historyRouter({
+    cleanUrlOnDispose: false,
+    windowTitle({ category, query }) {
+      const queryTitle = query ? `Results for "${query}"` : "Search";
+
+      if (category) {
+        return `${category} – ${queryTitle}`;
+      }
+
+      return queryTitle;
+    },
+
+    createURL({ qsModule, routeState, location }) {
+      const urlParts = location.href.match(/^(.*?)\/search/);
+      const baseUrl = `${urlParts ? urlParts[1] : ""}/`;
+
+      const categoryPath = routeState.category
+        ? `${getCategorySlug(routeState.category)}/`
+        : "";
+      const queryParameters = {};
+
+      if (routeState.query) {
+        queryParameters.query = encodeURIComponent(routeState.query);
+      }
+      if (routeState.page !== 1) {
+        queryParameters.page = routeState.page;
+      }
+      if (routeState.brands) {
+        queryParameters.brands = routeState.brands.map(encodeURIComponent);
+      }
+
+      const queryString = qsModule.stringify(queryParameters, {
+        addQueryPrefix: true,
+        arrayFormat: "repeat",
+      });
+
+      return `${baseUrl}search/${categoryPath}${queryString}`;
+    },
+
+    parseURL({ qsModule, location }) {
+      const pathnameMatches = location.pathname.match(/search\/(.*?)\/?$/);
+      const category = getCategoryName(
+        (pathnameMatches && pathnameMatches[1]) || ""
+      );
+      const {
+        query = "",
+        page,
+        brands = [],
+      } = qsModule.parse(location.search.slice(1));
+      // `qs` does not return an array when there's a single value.
+      const allBrands = Array.isArray(brands)
+        ? brands
+        : [brands].filter(Boolean);
+
+      return {
+        query: decodeURIComponent(query),
+        page,
+        brands: allBrands.map(decodeURIComponent),
+        category,
+      };
+    },
+  }),
+
+  stateMapping: {
+    stateToRoute(uiState) {
+      const indexUiState = uiState["instant_search"] || {};
+
+      return {
+        query: indexUiState.query,
+        page: indexUiState.page,
+        brands:
+          indexUiState.refinementList && indexUiState.refinementList.brand,
+        category: indexUiState.menu && indexUiState.menu.categories,
+      };
+    },
+
+    routeToState(routeState) {
+      return {
+        instant_search: {
+          query: routeState.query,
+          page: routeState.page,
+          menu: {
+            categories: routeState.category,
+          },
+          refinementList: {
+            brand: routeState.brands,
+          },
+        },
+      };
+    },
+  },
+};
+
+export default function useRouting() {
+  return routing;
+}
